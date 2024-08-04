9af97a4aded352acee8292d01a03967ba99e2439
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-04 15:20:49 +0200

    Our First Server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+if (process.env.NODE_ENV !== 'local') {
+    app.use(cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    }));
+} else {
+    app.use(cors({
+        credentials: true,
+    }));
+}
+
+const PORT = process.env.PORT || 5650;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
