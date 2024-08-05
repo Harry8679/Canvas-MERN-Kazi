49aac2191511520a6302e5d65af0206a3db54d4e
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-05 23:59:43 +0200

    Useless

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,3 @@ export default defineConfig({
     outDir: 'dist'
   }
 });
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
